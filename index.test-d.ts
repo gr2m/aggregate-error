187098986b47d804d397172237bcbf62c11162da
Gregor Martynus <gregor@martynus.net>
2020-08-04 11:22:45 -0700

    test(TypeScript): support custom Error type

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,14 +1,25 @@
-import {expectType} from 'tsd';
-import AggregateError = require('.');
+import { expectType } from "tsd";
+import AggregateError = require(".");
 
 const aggregateError = new AggregateError([
-	new Error('foo'),
-	{foo: 'bar'},
-	'bar'
+	new Error("foo"),
+	{ foo: "bar" },
+	"bar"
 ]);
 expectType<Iterable<Error>>(aggregateError);
 expectType<IterableIterator<Error>>(aggregateError[Symbol.iterator]());
 
 for (const error of aggregateError) {
 	expectType<Error>(error);
+}
+
+interface CustomError extends Error {
+	foo: string;
+}
+const customAggregateError = new AggregateError<CustomError>([
+	new Error("foo")
+]);
+
+for (const error of customAggregateError) {
+	expectType<string>(error.foo);
 }
